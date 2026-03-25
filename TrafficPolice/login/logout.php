bbb80c1e03eb6b3e9bd242154b60ee20b694c669
moulikaran213-boop <moulikaran213@gmail.com>
2026-03-25 13:27:21 +0530

    Delete TrafficPolice directory

--- TrafficPolice/login/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['pid']);
-unset($_SESSION['pname']);
-unset($_SESSION['uname']);
-header('location:../index.php');
-?>
--- TrafficPolice/print.php
+++
@@ -1,5 +0,0 @@
-<?php 
-session_start();
-     unset($_SESSION['uid']);
-     header('location:home.php');
- ?>
